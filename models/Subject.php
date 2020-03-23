52a4282ae7c551d200caf2f6366ea7628d7a9689
davichang <davichang@hotmail.com>
2020-03-23 11:05:27 +0800

    update SubjectModel

--- models/Subject.php
+++ models/Subject.php
@@ -8,9 +8,9 @@ use Yii;
  * This is the model class for table "subject".
  *
  * @property int $id
+ * @property int $subject_id 所属科目
  * @property string $title 书名或章节名
  * @property string $info 简介
- * @property int $is_chapter 是否章节：1是0否
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
  */
@@ -30,8 +30,8 @@ class Subject extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
+            [['subject_id', 'created_at', 'updated_at'], 'integer'],
             [['title', 'info', 'created_at', 'updated_at'], 'required'],
-            [['is_chapter', 'created_at', 'updated_at'], 'integer'],
             [['title'], 'string', 'max' => 100],
             [['info'], 'string', 'max' => 200],
         ];
@@ -44,9 +44,9 @@ class Subject extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
+            'subject_id' => 'Subject ID',
             'title' => 'Title',
             'info' => 'Info',
-            'is_chapter' => 'Is Chapter',
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
         ];
